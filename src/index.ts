e114607dbe5242613b43dbc141107fd971906548
ipinID <arif.ipincamp@gmail.com>
2023-03-13 00:36:13 +0700

    Setup ESLint, Prettier & Husky

--- src/index.ts
+++ src/index.ts
@@ -1,18 +1,20 @@
-import express, { Application, NextFunction, Request, Response } from "express";
+import express, { type Application, type NextFunction, type Request, type Response } from 'express'
 
-const app: Application = express();
-const port: Number = 5000;
+const app: Application = express()
+const port: number = 5000
 
-app.use("/", (req: Request, res: Response, next: NextFunction) => {
+app.use('/', (req: Request, res: Response, next: NextFunction) => {
   try {
     res.status(200).json({
       __code: 200,
       status: true,
-      result: "OK",
-    });
+      result: 'OK'
+    })
   } catch (error) {
-    next(error);
+    next(error)
   }
-});
+})
 
-app.listen(port, () => console.info("http://localhost:5000/"));
+app.listen(port, () => {
+  console.info('http://localhost:5000/')
+})
