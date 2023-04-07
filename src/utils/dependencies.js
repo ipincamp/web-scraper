bd96a5150897a6e9e5de30e7239a0a56388bcda0
ipinID <arif.ipincamp@gmail.com>
2023-04-07 14:15:50 +0700

    Deprecated
    - Moving separate module
    - Minimalist import usage

---
+++ src/utils/dependencies.js
@@ -0,0 +1,6 @@
+const cheerio = require("cheerio");
+const vm = require("vm");
+const FormData = require("form-data");
+const { default: axios } = require("axios");
+
+module.exports = { axios, cheerio, vm, FormData };
