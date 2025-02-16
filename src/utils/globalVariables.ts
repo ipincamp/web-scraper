b6926931cb83e02f32ba66dbaed025d2655062f6
ipincamp <support@nur-arifin.my.id>
2025-02-16 19:27:13 +0700

    Remove Snaptik and Ssstik service implementations and related types

--- src/utils/globalVariables.ts
+++ src/utils/globalVariables.ts
@@ -1,3 +1,3 @@
 export default {
-    userAgent: "Mozilla/5.0 (Linux; Android 10; SM-A107F) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/92.0.4515.159 Mobile Safari/537.36",
+    userAgent: "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/132.0.0.0 Safari/537.36",
 };
