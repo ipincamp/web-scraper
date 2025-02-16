31358472487b490ead30c2926ada7661a1387bf9
ipincamp <support@nur-arifin.my.id>
2025-02-16 19:28:39 +0700

    Add ts-node for TypeScript execution and create test script for TikTok media extraction

---
+++ example.ts
@@ -0,0 +1,16 @@
+import { TikTok } from "./src";
+
+(async () => {
+    /**
+     * Video: https://www.tiktok.com/@bg.andii/video/7140173156311960858
+     * Photo: https://www.tiktok.com/@areioutdoorgear_/photo/7471638638397328646
+     */
+    const tiktokURL = 'https://www.tiktok.com/@areioutdoorgear_/photo/7471638638397328646';
+    const tiktok = new TikTok(tiktokURL)
+
+    const snaptik = await tiktok.snaptik();
+    console.info(snaptik);
+
+    const ssstik = await tiktok.ssstik();
+    console.info(ssstik);
+})();
